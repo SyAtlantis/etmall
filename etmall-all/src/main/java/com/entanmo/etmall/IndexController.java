94fab2748b363c230b7ec2f76030164fb8ce76f1
SyAtlantis <1102960695@qq.com>
2019-07-31 17:23:16 +0800

    初始化项目

---
+++ etmall-all/src/main/java/com/entanmo/etmall/IndexController.java
@@ -0,0 +1,15 @@
+package com.entanmo.etmall;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello index";
+    }
+}
